d8ad16b252bd268b75ec59d67431d79b9b453fe0
pesche <pesche@gravenstein>
1998-10-10 21:15:01 +0200

    - Tabulator-Befehle in neues Tabulator-Untermenü verschoben - neues Suchen-Untermenu
    Emacs-Lisp 1998/10/10 21:15:01
    
    HW-Depot@15937 2003/02/07 00:05:02 :
    
    [git-p4: depot-paths = "//depot/pesche/home/,//depot/usr/local/share/emacs/site-lisp/": change = 812]

--- usr.local.share.emacs.site-lisp/pesche-menu.el
+++ usr.local.share.emacs.site-lisp/pesche-menu.el
@@ -11,22 +11,55 @@
 (setq menu-bar-final-items '(pesche-menu help-menu))
 
 ; und hier das Menu selbst (die Einträge geschiehen in umgekehrter Reihenfolge...
-(define-key menu-bar-pesche-menu [pesche-indent-spc]
-  '("Use Spaces" . (lambda()(interactive) (setq indent-tabs-mode nil))))
 
-(define-key menu-bar-pesche-menu [pesche-indent-tab]
-  '("Use Tabs"   . (lambda()(interactive) (setq indent-tabs-mode t))))
+; Tabulator-Untermenü einhängen
+(define-key menu-bar-pesche-menu [tab-menu]
+  '("Tabulators" . pesche-tab-menu))
 
-(define-key menu-bar-pesche-menu [pesche-separator-4]
+(define-key menu-bar-pesche-menu [pesche-separator-5]
   '("--"))
 
-(define-key menu-bar-pesche-menu [pesche-tab-8]
+; Suchen-Untermenü einhängen
+(define-key menu-bar-pesche-menu [find-menu]
+  '("Find special" . pesche-find-menu))
+
+; Suchen Untermenü -------------------------------------------------------------
+(defvar pesche-find-menu (make-sparse-keymap "Find-menu"))
+(define-key global-map [menu-bar pesche-menu find-menu]
+  (cons "Find special" pesche-find-menu))
+
+(define-key pesche-find-menu [pesche-find-trails]
+  '("Trailing Whitespace" .
+    (lambda()(interactive)
+      (nonincremental-re-search-forward "[ \t]$"))))
+
+;; findet /**/, /*-*/, /*--*/ und /*---*/
+(define-key pesche-find-menu [pesche-find-annot]
+  '("Annotations /*-*/" .
+    (lambda()(interactive)
+      (nonincremental-re-search-forward "/\\*-?-?-?\\*/"))))
+
+; Tabulator Untermenü ----------------------------------------------------------
+(defvar pesche-tab-menu (make-sparse-keymap "Tab-menu"))
+(define-key global-map [menu-bar pesche-menu tab-menu]
+  (cons "Tabulators" pesche-tab-menu))
+
+(define-key pesche-tab-menu [pesche-indent-spc]
+  '("Use Spaces" . (lambda()(interactive) (setq indent-tabs-mode nil))))
+
+(define-key pesche-tab-menu [pesche-indent-tab]
+  '("Use Tabs"   . (lambda()(interactive) (setq indent-tabs-mode t))))
+
+(define-key pesche-tab-menu [pesche-separator-4]
+  '("--"))
+
+(define-key pesche-tab-menu [pesche-tab-8]
   '("Tabstop 8" . (lambda()(interactive) (setq tab-width 8) (redraw-display))))
 
-(define-key menu-bar-pesche-menu [pesche-tab-4]
+(define-key pesche-tab-menu [pesche-tab-4]
   '("Tabstop 4" . (lambda()(interactive) (setq tab-width 4) (redraw-display))))
 
-(define-key menu-bar-pesche-menu [pesche-entab-region]
+(define-key pesche-tab-menu [pesche-entab-region]
   '("Entab Region" . tabify))
 
 ; (define-key menu-bar-pesche-menu [pesche-entab-indent-region]
@@ -34,13 +67,13 @@
 ;                               (let ((tabify-regexp "^[ \t]+"))
 ;                                 (tabify)))))
 
-(define-key menu-bar-pesche-menu [pesche-entab-buffer]
+(define-key pesche-tab-menu [pesche-entab-buffer]
   '("Entab Buffer" . (lambda()(interactive) (tabify (point-min) (point-max)))))
 
-(define-key menu-bar-pesche-menu [pesche-detab-region]
+(define-key pesche-tab-menu [pesche-detab-region]
   '("Detab Region" . untabify))
 
-(define-key menu-bar-pesche-menu [pesche-detab-buffer]
+(define-key pesche-tab-menu [pesche-detab-buffer]
   '("Detab Buffer" . (lambda()(interactive) (untabify (point-min) (point-max)))))
 
 (define-key menu-bar-pesche-menu [pesche-separator-3]

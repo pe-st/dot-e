d905c9bd0fe42ff6455f367d225c1be4f78ab0da
pesche <pesche@gravenstein>
2002-01-15 21:48:41 +0100

    Bei Emacs 21.1 jetzt dabei
    Emacs-Lisp 2002/01/15 21:48:41
    
    HW-Depot@15983 2003/02/07 00:08:49 :
    
    [git-p4: depot-paths = "//depot/pesche/home/,//depot/usr/local/share/emacs/site-lisp/": change = 857]

--- usr.local.share.emacs.site-lisp/subdirs.el
+++ usr.local.share.emacs.site-lisp/subdirs.el
@@ -1,14 +1,2 @@
-;;     $Source: g:/archiv/cvsroot/site-lisp/subdirs.el,v $
-;;   $Revision: 1.1 $
-;;       $Date: 1999/08/10 12:15:33 $
-;;     $Author: pesche $
-;;
-;; this is a workaround for NTEmacs 20.3 and 20.4 to obtain a behavior
-;; documented in the NEWS file
-;;
-;; add all subdirectories of this file's directory to the load path
-;; (if this file resides in a dir in load-path, it gets loaded
-;; automatically; see normal-top-level in startup.el)
-(normal-top-level-add-subdirs-to-load-path)
-
-;; eof
+(if (fboundp 'normal-top-level-add-subdirs-to-load-path)
+    (normal-top-level-add-subdirs-to-load-path))
